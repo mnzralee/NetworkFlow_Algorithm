f9bc2cfe790d008742842ac565db11b25b45df50
Manazir Ali <mnzr.alee@gmail.com>
2025-05-22 16:19:12 +0530

    Update EdmondsKarp.java

--- src/algorithm/EdmondsKarp.java
+++ src/algorithm/EdmondsKarp.java
@@ -1,4 +1,30 @@
 package algorithm;
 
+import data_structures.AdjacencyList;
+
 public class EdmondsKarp {
+
+    private AdjacencyList graph;
+    private int source;
+    private int sink;
+    private int maxFlow;
+
+    /*
+     * Constructor for the Algorithm.
+     *
+     * @param graph The AdjacencyList representing the Network.
+     * @param source The Source Node.
+     * @param sink The Sink Node.
+     */
+    public EdmondsKarp(AdjacencyList graph, int source, int sink) {
+        this.graph = graph;
+        this.source = source;
+        this.sink = sink;
+        this.maxFlow = 0; // Initially the flow is zero to start with.
+    }
+
+    public void runEdmondsKarp() {
+        int numNodes = graph.getNumNodes();
+        int[] parent = new int[numNodes];
+    }
 }
